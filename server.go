a0623c729d047de13a0015f2c981a48fd8a0a481
frank cuthbertson <fccuthbertson@gmail.com>
2022-05-19 15:02:08 -0700

    reworked file server. renamed ./template -> ./assets

--- server.go
+++ server.go
@@ -7,18 +7,27 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./template")))
+	mux := http.NewServeMux()
+	staticHandler := http.FileServer(http.Dir("./assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets", staticHandler))
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/contact", contact)
 
-	port := ":5000"
+	port := ":8080"
 	fmt.Println("Server is running on port" + port)
 
 	// Start server on port specified above
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(port, mux))
 }
 
-func Serve(dirname string, port string) error {
-	fs := http.FileServer(http.Dir(dirname))
-	http.Handle("/", fs)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	http.ServeFile(w, r, "./assets/index.html")
+}
 
-	return http.ListenAndServe(":"+port, nil)
+func contact(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println(request.Form)
 }
