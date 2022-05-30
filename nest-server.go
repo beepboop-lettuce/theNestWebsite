10ff0258b3ba80a25ee88f26f1bb86de294a34bc
frank cuthbertson <fccuthbertson@gmail.com>
2022-05-30 16:45:42 -0500

    forcing https redirect

--- nest-server.go
+++ nest-server.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/smtp"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 )
 
@@ -21,6 +23,9 @@ func reloadable() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Scheme != "https" {
+		log.Printf("scheme: %v", r.URL.String())
+	}
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -29,7 +34,47 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func contact(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println(request.Form)
+	err := request.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(request.Form)
+	form := request.Form
+	// send to leadership@nestatandersonmill.com
+	smtpServer := getEnv("NEST_EMAIL_SERVER")
+	user := "fccuthbertson@gmail.com"
+	//user := getEnv("CONTACT_FORM_USER")
+	//pwd := getEnv("CONTACT_FORM_PWD")
+	pwd := "uenuirnjyvhtasuf"
+	recipient := getEnv("CONTACT_FORM_TO")
+	sender := getEnv("CONTACT_FORM_FROM")
+
+	auth := smtp.PlainAuth("", user, pwd, smtpServer)
+	to := []string{recipient}
+	from := sender
+	name := form.Get("name")
+	email := form.Get("email")
+	message := form.Get("message")
+	msg := fmt.Sprintf("From: %v %v\n"+
+		"Subject: New Nest Contact!"+
+		"%v", name, email, message)
+	msgBytes := []byte(msg)
+	err = smtp.SendMail(smtpServer+":587", auth, from, to, msgBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func getEnv(k string) string {
+	v, ok := os.LookupEnv(k)
+	if !ok {
+		log.Printf("env var: %v not set", k)
+		return ""
+	}
+	log.Printf("key: %v val: %v", k, v)
+	return v
 }
 
 func register() *http.ServeMux {
@@ -43,35 +88,27 @@ func register() *http.ServeMux {
 }
 
 func listen(mux *http.ServeMux) {
-
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Println("$PORT not set")
 		os.Exit(1)
 	}
-	log.Println("Server starting on port: " + port)
-
-	if os.Getenv("ENV") != "development" {
-
-		//cert := os.Getenv("NEST_CERT")
-		//if cert == "" {
-		//	log.Println("$NEST_CERT not set")
-		//	os.Exit(1)
-		//}
-		//
-		//key := os.Getenv("NEST_KEY")
-		//if key == "" {
-		//	log.Println("$NEST_KEY not set")
-		//	os.Exit(1)
-		//}
-		// Start server on $PORT
-		log.Println("using TLS")
-		log.Fatal(http.ListenAndServeTLS(":8443", "./server.crt", "./server.key", mux))
-	} else {
-		log.Fatal(http.ListenAndServe(":"+port, mux))
-	}
-
+	go func() {
+		err := http.ListenAndServeTLS(":8443", "server.crt", "server.key", mux)
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+	log.Println("listening on : " + port)
+	log.Fatal(http.ListenAndServe(":"+port, http.HandlerFunc(redirectToTLS)))
+}
+func redirectToTLS(w http.ResponseWriter, r *http.Request) {
+	host := r.Host
+	// need to split host from port
+	s := strings.Split(host, ":")
+	log.Printf("redirect host: %v", s[0])
+	http.Redirect(w, r, "https://"+s[0]+":8443"+r.RequestURI, http.StatusMovedPermanently)
 }
 
 func main() {
